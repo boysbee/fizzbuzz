06b71c1d5e5e538eb734df9f83177452861c8f10
nattaporn.c <nattaporn.c@dotography.com>
2016-10-05 15:34:54 +0700

    Add test buzz case.

--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -9,8 +9,47 @@ class FizzBuzzTest extends FunSuite with Matchers {
     val result = FizzBuzz.getResult(number)
     result should be("fizz")
   }
+  test("input number 5 should return \"buzz\"") {
+    val number = 5
+    val result = FizzBuzz.getResult(number)
+    result should    be("buzz")
+  }
+  test("input number 1 should return same value") {
+    val number = 1
+    val result = FizzBuzz.getResult(number)
+    result should  be("1")
+  }
+  test("input number 6 should return \"fizz\"") {
+    val number = 6
+    val result = FizzBuzz.getResult(number)
+    result should  be("fizz")
+  }
+  test("input number 7 should return \"7\"") {
+    val number = 7
+    val result = FizzBuzz.getResult(number)
+    result should  be("7")
+  }
+  test("input number 9 should return \"fizz\"") {
+    val number = 9
+    val result = FizzBuzz.getResult(number)
+    result should  be("fizz")
+  }
+  test("input number 10 should return \"buzz\"") {
+    val number = 10
+    val result = FizzBuzz.getResult(number)
+    result should  be("buzz")
+  }
   object FizzBuzz {
-    def getResult(number: Int):String = "fizz"
+    def getResult(number: Int):String = {
+      if(number % 5 == 0) {
+        return "buzz"
+      }
+
+      if(number % 3 == 0) {
+        return "fizz"
+      }
+      number.toString
+    }
   }
 
 }
